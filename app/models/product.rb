50611a28cd91dd8e4ba9764b07e0ae9b4197e8c6
papua <nakao_shinichirou@aratana.jp>
2014-08-19 17:42:33 +0900

    move label creation logic from LabelCreator to Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -15,10 +15,41 @@ class Product < ActiveRecord::Base
   }
 
   def get_genre_display_obj
-    return Genre.all.pluck(:name, :id)
+    return Genre.pluck(:name, :id)
   end
 
   def get_category_display_obj
     return CATEGORYS.map{|k, v| [v, k]}
   end
+
+  def get_next_label
+    return count_up get_current_label
+  end
+
+  def get_current_label
+    first = Product.where(genre_id: self.genre_id).where(category: self.category).order("label DESC").first
+    if first
+      return first.label
+    else
+     return is_free ? Genre.where(id: self.genre_id).pluck(:free_label).first : 
+        Genre.where(id: self.genre_id).pluck(:pay_label).first
+    end
+  end
+  private :get_current_label
+
+  def count_up(label)
+    if is_free
+      counter = label[2, 4].to_i(10)
+      return "F" +  self.genre_id.to_s + format("%04d", counter + 1)
+    else
+      counter = label[1, 3].hex
+      return self.genre_id.to_s + format("%03x", counter + 1)
+    end
+  end
+  private :count_up
+
+  def is_free
+    return self.category == "0"
+  end
+  private :is_free
 end

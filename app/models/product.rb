821428d8110a0e172020396639a3964bb27a9152
papua <nakao_shinichirou@aratana.jp>
2014-08-25 10:00:39 +0900

    use custom validate method for format check.

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,31 @@ class Product < ActiveRecord::Base
   has_one :genre
 
   before_create :default_value
-  after_create :save_files
+  before_save :save_files
+  
+  attr_accessor :thumbnail_file, :exported_file
+  
+  validates :name, :genre_id,:category, :thumbnail_file, :exported_file,  presence: {message: "すべての項目が入力必須です。"}
+  validates :name, uniqueness: {message: "この表示名は既に使用されています。"}
+  validates :genre_id, numericality: true
+  validate :thumbnail_file_format_check
+  validate :exported_file_format_check
 
+  def thumbnail_file_format_check
+    if (thumbnail_file && thumbnail_file.original_filename !~ %r{\.(bmp|gif|jpg|jpeg|png)\z}i)
+      errors.add(:thumbnail_file, "サムネイル画像の拡張子が不正です。")
+    end
+  end
+
+  def exported_file_format_check
+    if (exported_file && exported_file.original_filename !~ %r{\.(sklp|skbn)\z}i)
+    errors.add(:exported_file, "クスポートデータの拡張子が不正です。") 
+    end
+  end
+  
   def default_value
     self.version ||= "1"
   end
-
-  attr_accessor :thumbnail_file, :exported_file
   
   CATEGORYS = {
     "0" => "無料",
@@ -67,26 +85,13 @@ class Product < ActiveRecord::Base
     end
 
     def save_files
-      thumb_exts = [
-        ".jpg",
-        ".jpeg",
-        ".png",
-        ".gif",
-        ".bmp"
-      ]
-
       self.label = self.next_label
       self.thumbnail_name = self.label + File.extname(self.thumbnail_file.original_filename)
       self.exported_name = self.label + File.extname(self.exported_file.original_filename)
-      self.save
 
       base = Rails.root.join("var")
       save_file(base.join("thumb").join(self.thumbnail_name).to_s, self.thumbnail_file)
       save_file(base.join("data").join(self.exported_name).to_s, self.exported_file)
-    end
-
-    def safe_ext?(path, exts)
-      return exts.include?(File.extname(path))
     end
 
     def save_file(path, file)
